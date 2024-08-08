cd7216e344033ad79f8b9dea2f1bb92aa467ce93
beatrizgdc <beatrizgdcevaio@gmail.com>
2024-08-08 19:19:10 -0300

    feat: initial config

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { server } from './server/server.ts';
+
+server.listen(8084, () => console.log('App rodando!'));
---
+++ src/server/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const server = express();
+
+server.get('/', (_, res) => {
+
+
+    return res.send('Olá dev!');
+});
+
+export { server };
