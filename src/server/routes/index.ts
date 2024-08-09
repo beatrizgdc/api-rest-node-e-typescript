82dc3cd788cb7b600bec280e3f15e44d334ea68c
beatrizgdc <beatrizgdcevaio@gmail.com>
2024-08-09 15:21:44 -0300

    feat: status code and tsc

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,21 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
 
-router.get('/', (_, res) => {
 
-    return res.send('Olá dev!');
+router.get('/', (_, res) => {
+  return res.send('Olá, DEV!');
 });
+
+
+router.post('/teste', (req, res) => {
+  // console.log(req);
+
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+
+
 
 export { router };
